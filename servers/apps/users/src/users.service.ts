8917f52398602d69921428f1cb122a7e2a43cdde
laiba-saeed <laibasaeed538@gmail.com>
2024-09-24 15:48:52 +0500

    Update users.service.ts

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,40 @@
+/* Business Logic*/ 
 import { Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    // private readonly prisma 
+    private readonly configService: ConfigService,
+  ) {}
+
+  // Register User
+  async register (registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+    const user = {
+      name, 
+      email,
+      password,
+    }
+    return user;
+  }
+
+  // Login User
+  async login (loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = {
+      email,
+      password,
+    }
+    return user;
+  }
+
+  // Get all Users
+  async getUsers() {
+    const users = [];
+    return users;
   }
 }
