402a2a3b0add96471ecbd46713ee7e9f32a288bc
laiba-saeed <laibasaeed538@gmail.com>
2024-10-17 13:32:34 +0500

    Resolver

--- servers/apps/users/src/user.resolver.ts
+++ servers/apps/users/src/user.resolver.ts
@@ -2,37 +2,35 @@
 and the application logic, handling how data is retrieved, created, or updated. */
 
 // import { UseFilters } from "@nestjs/common";
-import { Args, Query, Mutation, Resolver } from "@nestjs/graphql";
-import { UsersService } from "./users.service";
-import { RegisterDto } from "./dto/user.dto";
-import { RegisterResponse } from "./types/user.types";
-import { BadRequestException } from "@nestjs/common";
-import { User } from "./entities/user.entity";
+import { Args, Query, Mutation, Resolver } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { RegisterDto } from './dto/user.dto';
+import { RegisterResponse } from './types/user.types';
+import { BadRequestException } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Resolver('User')
 // @UseFilters
 export class UserResolver {
-    constructor (
-        private readonly userService: UsersService
-    ) {}
+  constructor(private readonly userService: UsersService) {}
 
-    /* @Mutation is used to define operations that change or manipulate data (create, update, delete) 
+  /* @Mutation is used to define operations that change or manipulate data (create, update, delete) 
     in a GraphQL API within a NestJS application. */
-    @Mutation(() => RegisterResponse)
-    async register(
-        @Args('registerInput') registerDto: RegisterDto,
-    ) : Promise<RegisterResponse> {
-        if (registerDto.name || registerDto.email || registerDto.password) {
-            throw new BadRequestException('Please fill all the fields.')
-        }
-
-        const user = await this.userService.register(registerDto);
-
-        return { user };
+  @Mutation(() => RegisterResponse)
+  async register(
+    @Args('registerInput') registerDto: RegisterDto,
+  ): Promise<RegisterResponse> {
+    if (registerDto.name || registerDto.email || registerDto.password) {
+      throw new BadRequestException('Please fill all the fields.');
     }
 
-    @Query(() => [User])
-    async getUsers() {
-        return this.userService.getUsers();
-    }
+    const user = await this.userService.register(registerDto);
+
+    return { user };
+  }
+
+  @Query(() => [User])
+  async getUsers() {
+    return this.userService.getUsers();
+  }
 }
